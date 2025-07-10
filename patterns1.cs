2b2af353d42ee9515e594ed295e68d5da3a0dd15
kpaghadal <kpaghadal45@gmail.com>
2025-07-10 11:21:03 +0530

    added

--- patterns1.cs
+++ patterns1.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            //Output:
+            //Output:                                     //program1
             //*****
             //****
             //***
@@ -15,7 +15,7 @@ namespace ConsoleApp2
             //*
             //    for (int i = 1; i <= 5; i++)
             //    {
-            //        for (int j = i; j <= 5; j++)
+            //        for (int j = i; j <= 5; j++)     
             //        {
             //            Console.Write("*");
             //        }
@@ -23,7 +23,7 @@ namespace ConsoleApp2
             //    }
 
 
-            //Output:
+            //Output:                                 //program2
             //*
             //**
             //***
@@ -41,7 +41,7 @@ namespace ConsoleApp2
 
 
 
-            //Output:
+            //Output:                                    //program3
             //*****
             //*****
             //*****
@@ -59,7 +59,7 @@ namespace ConsoleApp2
 
 
 
-            //Output:
+            //Output:                                   //program4
             //1
             //12
             //123
@@ -76,7 +76,7 @@ namespace ConsoleApp2
 
 
 
-            //Output:
+            //Output:                                  //program5
             //    *
             //    **
             //    ***
@@ -103,31 +103,75 @@ namespace ConsoleApp2
 
 
 
-            //Output:
+            //Output:                                 //program6
             //    *
             //   **
             //  ***
             // ****
             //*****
-            for (int i = 1; i <= 5; i++)
-            {
-                for (int j = 1; j <= 5; j++)
-                {
-                    if (j <= 5 - i)
-                        Console.Write(" ");
-                    else
-                        Console.Write("*");
-                }
-                Console.WriteLine();
-            }
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    for (int j = 1; j <= 5; j++)
+            //    {
+            //        if (j <= 5 - i)
+            //            Console.Write(" ");
+            //        else
+            //            Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Output:                                 //program7
+            //it will print 1 to 10 table
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    for (int j = 1; j <= 10; j++)
+            //    {
+            //        int k = i * j;
+            //        Console.Write(i + "*" + j + "=" + k);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+
+            //int a = 5;
+            //int b = 5;
+            //int res = a++ + b++ + a++;                 ////output16 in this 5+5+6
+            //Console.WriteLine(res);
+
+            //int a = 5;
+            //int b = 5;
+            //int res = a++ + b++;                      ////output10
+            //Console.WriteLine(res);
+
+
+            //int a = 5;
+            //int b = 5;
+            //int result = a++;                         ////output5
+            //Console.WriteLine(result);
+
+
+            //int a = 5;
+            //int b = 5;
+            //int res = a++ + b++ + a++ + b++;          ////output22 5+6+5+6
+            //Console.WriteLine(res);
+
+
+            int a = 5;
+            int b = 5;
+            int res = ++a + b++ + b++ + a++ + ++a + ++b + a++;          ////output47 6+5+6+6+8+8+8
+            Console.WriteLine(res);
+
+            
 
 
 
 
 
         }
+    }
 }
-}
+
 
 
 
